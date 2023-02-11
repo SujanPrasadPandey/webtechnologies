00cb18b483afaedf71deb32c584cd191b0b51ad8
SujanPrasadPandey <88sujanpandey@gmail.com>
2023-02-11 21:06:26 +0545

    Portfolio Final

---
+++ Portfolio2/main.js
@@ -0,0 +1,26 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
+const mobileMenu = document.querySelector('.mobile-nav');
+
+hamburger.addEventListener("click", mobileMenu);
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+
+function mobileMenu() {
+    mobileMenu.classList.toggle('active');
+    
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+
+/*
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
